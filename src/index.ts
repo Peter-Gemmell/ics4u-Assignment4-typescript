16a2a23526c885c14ab18c49f6132e0d3781f89a
peter.gemmell <peter.gemmell@mths.ca>
2022-11-20 01:23:58 +0000

    ulysses

---
+++ src/index.ts
@@ -0,0 +1,54 @@
+/*
+ * This program ...
+ *
+ * @author  Malcolm Tompkins
+ * @version 1.0
+ * @since   2021-01-02
+ */
+
+'use strict'
+
+import promptSync from 'prompt-sync'
+const prompt = promptSync()
+
+/**
+ *
+ * Function of BinarySearch
+ *
+ * @param {number} integer number of disks
+ * @param {number} spacesNum first peg
+ */
+function asteristics(integer: number, spacesNum: number = 0) {
+  // Generate asteristics.
+  let output: string = ''
+
+  for (let counter: number = 0; counter < spacesNum; counter++) {
+    output += ' '
+  }
+
+  for (let counter1: number = 0; counter1 < integer; counter1++) {
+    output += '* '
+  }
+
+  console.log(output)
+
+  if (integer > 1) {
+    asteristics(integer - 1, spacesNum + 1)
+  }
+  console.log(output)
+}
+
+const input = prompt('Insert height of hourglass: ')
+try {
+  const inputNum = parseInt(input)
+
+  if (isNaN(inputNum)) {
+    console.log('This is not a number.')
+  } else {
+    asteristics(inputNum)
+  }
+} catch (e) {
+  console.log('\nThis is not a number')
+}
+
+console.log('Done.')
